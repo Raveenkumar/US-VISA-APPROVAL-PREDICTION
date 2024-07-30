4670831aa7ddb01db83190e3df8aa498861b79a3
Raveenkumar <jraveen1997@gmail.com>
2024-07-30 11:50:02 +0530

    "setup completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = "US-VISA",
+    version = "1.0",
+    author = "Raveenkumar",
+    author_email = "jraveen1997@gmail.com",
+    packages= find_packages()
+)
